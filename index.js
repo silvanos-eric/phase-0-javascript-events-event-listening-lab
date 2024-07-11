ddf7e4161f7a0c6508fb5f2d7a7d06be4c00e23b
Silvanos Eric <ericoderito@gmail.com>
2024-07-11 17:04:31 +0300

    feat: add click event listener to input and refactor tests
    - Implement an event listener in index.js that triggers an alert when the input element is clicked.
    - Define the input element using `document.querySelector` for targeted selection.
    - Refactor test setup in indexTest.js by cleaning up syntax and ensuring consistency in string
      quotes.
    - Update tests to check the binding of the event listener to the input element effectively.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+  input.addEventListener("click", () => alert("I was clicked!"));
 }
+
+const input = document.querySelector("input#button");
+addingEventListener();
